fe33ea1ff29b1681d495e6cc423c93a994cff89e
ken556621 <53808676+ken556621@users.noreply.github.com>
2019-10-08 15:32:53 +0800

    Update fizzbuzz_spec.js

--- test/fizzbuzz_spec.js
+++ test/fizzbuzz_spec.js
@@ -1,14 +1,4 @@
-// var should = chai.should()
-// describe('function add', function() {
-//   it('should add two number and return result', function() {
-//     var result = add(2, 1)
-//     result.should.be.equal(3)
-//   })
-//   it('string inputs should be illegal', function() {
-//     var result = add('2', '1')
-//     result.should.be.equal('something wrong!!')
-//   })
-// })
+
 
 var should = chai.should();
 describe('function fizzBuzz', function(){
